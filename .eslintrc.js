2955436e57f135e6b28bc61874295b360329136f
Asjas <asjas@outlook.com>
2019-09-27 11:59:31 +0200

    Setup Eslint and Prettier with pre-commit hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,84 +1,102 @@
 module.exports = {
-  "extends": [
-    "airbnb-base",
-    "prettier",
-  ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 8,
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "impliedStrict": true,
-      "classes": true
-    }
+  extends: ['airbnb'],
+  plugins: ['react-hooks'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      impliedStrict: true,
+      classes: true,
+    },
   },
-  "env": {
-    "browser": true,
-    "node": true,
+  env: {
+    browser: true,
   },
-  "rules": {
-    "no-unused-vars": [
+  rules: {
+    'no-debugger': 0,
+    'no-alert': 0,
+    'no-unused-vars': [
       1,
       {
-        "argsIgnorePattern": "res|next|^err"
-      }
+        argsIgnorePattern: 'res|next|^err',
+      },
     ],
-    "arrow-body-style": [
-      2,
-      "as-needed"
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
     ],
-    "no-param-reassign": [
-      2,
-    ],
-    "no-console": 0,
-    "import/prefer-default-export": 0,
-    "import": 0,
-    "no-plusplus": ["error", {
-      "allowForLoopAfterthoughts": true
-    }],
-    "class-methods-use-this": ["error", {
-      "exceptMethods": ["render"]
-    }],
-    "func-names": 0,
-    "space-before-function-paren": 0,
-    "comma-dangle": 0,
-    "max-len": 0,
-    "import/extensions": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "radix": 0,
-    "no-shadow": [
+    'arrow-body-style': [2, 'as-needed'],
+    'arrow-parens': 0,
+    'object-curly-newline': 0,
+    'operator-linebreak': 0,
+    'no-unused-expressions': [
       2,
       {
-        "hoist": "all",
-        "allow": [
-          "resolve",
-          "reject",
-          "done",
-          "next",
-          "err",
-          "error"
-        ]
-      }
+        allowTaggedTemplates: true,
+      },
     ],
-    "quotes": [
+    'no-param-reassign': [
       2,
-      "single",
       {
-        "avoidEscape": true,
-        "allowTemplateLiterals": true
-      }
+        props: false,
+      },
     ],
-    "prettier/prettier": [
-      "error",
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    'import/no-extraneous-dependencies': 0,
+    import: 0,
+    'func-names': 0,
+    'no-use-before-define': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'global-require': 0,
+    'no-shadow': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    'react/display-name': 1,
+    'react/no-array-index-key': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/require-default-props': 0,
+    'react/forbid-prop-types': 0,
+    'react/no-unescaped-entities': 0,
+    'react/sort-comp': 0,
+    'react/jsx-max-props-per-line': [
+      2,
       {
-        "trailingComma": "all",
-        "singleQuote": true,
-        "printWidth": 120,
-      }
+        maximum: 1,
+        when: 'multiline',
+      },
     ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    radix: 0,
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'jsx-a11y/accessible-emoji': 0,
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['invalidHref'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
-  "plugins": [
-    "prettier"
-  ]
-}
+};
